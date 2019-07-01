43f87db41776b41cf1b219a1e9d57ff02a28ec1c
Sally Haefling <sallyhaefling@gmail.com>
2019-07-01 17:48:19 -0600

    Setup port and variables

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
-// const express = require('express');
-// const port = 3000;
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
-// const app = express();
+// app.use(express.json());
+// require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.set('port', process.env.PORT || 3001);
+
+// const express = require('express');
 
 // app.use(express.json());
 
 // app.listen(process.env.PORT || port, () => {
-//   console.log(`App is running on ${port}`)
+//   console.log(`App is running 😃 on ${port}`)
 // });
+
+app.listen(app.get('port'), () => console.log(`App is running 😃 on port ${app.get('port')}`));
+
+
 //get for all projects
 //get for single projects
 //get for all palettes
