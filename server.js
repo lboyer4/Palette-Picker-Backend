413daee11812d6d81875a159afbd59ba40ba5be9
Sally Haefling <sallyhaefling@gmail.com>
2019-07-02 18:38:17 -0600

    Add get endpoint for project id

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-
-// require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
 
+// app.use(express.json());
+// require('dotenv').config();
 
 app.set('port', process.env.PORT || 3001);
+
 app.listen(app.get('port'), () => console.log(`App is running 😃 on port ${app.get('port')}`));
 
-// app.locals.projects = [
-//   {
-//     name: 'Sally'
-//   },
-//   {
-//     name: 'Lauren'
-//   }
-// ]
-
-//get for all projects and all palettes:
-
-app.get('/', (request,response) => {
+app.get('/api/v1/project', (request, response) => {
   database('project').select()
   .then(project => {
     return response.status(200).json(project);
@@ -34,6 +23,20 @@ app.get('/', (request,response) => {
   })
 });
 
+app.get('/api/v1/project/:id', (request, response) => {
+  database('project').where('id', request.params.id)
+  .select()
+  .then(project => {
+    if(project.length) {
+      return response.status(200).json(project)
+    } else {
+      return response.status(404).json({ error: `Couldn't find project with id: ${request.params.id}` })
+    }
+  })
+  .catch(error => {
+    return response.status(500).json({ error });
+  })
+});
 
 //get for single projects /project/:id
 //get for all palettes /palettes
