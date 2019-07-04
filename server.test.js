5f2c22e84b061d09760d565834c2678e10586fd2
Sally Haefling <sallyhaefling@gmail.com>
2019-07-03 18:06:58 -0600

    Add pseudocode for get endpoint test and 200 status code

--- server.test.js
+++ server.test.js
@@ -1,7 +1,22 @@
+// import request from 'supertest'
+// import app from './app'
 
+// const environment = process.env.NODE_ENV || 'test'
+// const configuration = require('./knexfile')[environment]
+// const database = require('knex')(configuration)
 
-describe('this test is  a fake', () => {
+describe('Server', () => {
 	it('should have', () => {
 
 	})
 })
+
+//GET /project:
+
+//200 status code: it should return 200 status, async, create variable of response and await the request from app to get the project DB, expect response.status to be 200
+
+//create describe block for GET /project, should return every project in the DB, async
+	//setup: create variable for expectedProject and assign to await the database of project that is selected
+	//execution: create variable for response that awaits the request from app to get the project DB, create variable of project assigned to response.body
+	//expectation: expect project response.body to equal expectedProject
+	
