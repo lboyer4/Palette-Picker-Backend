f5aeac6314f652744f0a9713d1ab860441302dcb
Sally Haefling <sallyhaefling@gmail.com>
2019-07-05 12:18:41 -0600

    Add testing setup
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,18 @@ module.exports = {
       }
   },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/project_test',
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true,
+  },
+
   production: {
   client: 'pg',
   connection: process.env.DATABASE_URL + `?ssl=true`,
