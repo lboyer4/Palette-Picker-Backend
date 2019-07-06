c8e3b3aaad1394294d090990bbedd80a9eecd299
Sally Haefling <sallyhaefling@gmail.com>
2019-07-06 09:30:26 -0600

    Add tests for delete and error handlings

--- app.test.js
+++ app.test.js
@@ -34,6 +34,7 @@ describe('Server', () => {
 			const response = await request(app).get('/api/v1/project');
 			const result = response.body;
 
+			expect(response.status).toBe(200);
 			expect(result).toEqual(expectedProject);
     });
 	});
@@ -41,23 +42,25 @@ describe('Server', () => {
 	//GET /project/:id:
 
 		describe('GET /api/v1/project/:id', () => {
-			it.skip('should return a single project with the id in the params', async () => {
+			it('should return a single project with the id in the params', async () => {
 			const expectedProject = await database('project').first();
 			const id = expectedProject.id;
 
 			const response = await request(app).get(`/api/v1/project/${id}`);
-			const result = response.body;
-
+			const result = response.body[0];
+			
+			expect(response.status).toBe(200);
 			expect(result.name).toEqual(expectedProject.name);
 		});
 
 		it('should respond with an error if there was no project with id that matches the request', async () => {
-			const response = await request(app).get('/api/v1/project/999');
-			const result = response.body;
-
 			const expected = {
 				error: 'Couldn\'t find project with id: 999'
 			};
+
+			const response = await request(app).get('/api/v1/project/999');
+			const result = response.body;
+
 			expect(result).toEqual(expected);
 			expect(response.status).toBe(404);
 		});
@@ -67,32 +70,78 @@ describe('Server', () => {
 	
 	describe('POST /api/v1/project/', () => {
 		it('should post a new project to the DB', async () => {
-			
 			const newProject = {
 				"name": "project 11111"
 			}
+
 			const response = await request(app).post('/api/v1/project').send(newProject)
 			const id = response.body.id
 			const project = await database('project').where('id', id).first()
+
+			expect(response.status).toBe(201);
 			expect(newProject.name).toEqual(project.name)
-		})
+		});
+
+		it('should reject post if required parameter is invalid or not recieved', async () => {
+			const expectedError = {
+				error: `Expected format: { name: <String> } You're missing \"name\" property.`
+			};
+
+			const responseInvalidParam = await request(app).post('/api/v1/project');
+
+			expect(responseInvalidParam.status).toBe(422);
+			expect(responseInvalidParam.body).toEqual(expectedError);
+		});
 	})
 
 	//PUT /project/:id:
 
 	describe('PUT /api/v1/project/:id', () => {
-		it.skip('should update the current project with new information', async () => {
-
+		it('should update the current project with new information', async () => {
 			let updatedProject = await database('project').first()
-			const id = updatedProject
+			const id = updatedProject.id
 
 			const response = await request(app).put(`/api/v1/project/${id}`).send(updatedProject)
-			const project = await database('project').where({ id: id }).first()
+			const project = await database('project').where({ id: id }).first();
 
-			expect(updatedProject).toEqual(project)
+			expect(response.status).toBe(200);
+			expect(updatedProject).toEqual(project);
 		})
+
+		it('should respond with an error if project does not exist', async () => {
+			const updatedName = {name: 'akjehkqwhr'}
+			const error = {
+				error: 'Couldn\'t update: Project does not exist'
+			}
+
+			const response = await request(app).put('/api/v1/project/-1').send(updatedName);
+
+			expect(response.status).toBe(404);
+			expect(response.body).toEqual(error);
+		});
+	})
+	
+	//DELETE /project/:id:
+	
+	describe('DELETE /api/v1/project/:id', () => {
+		it('should delete a project using the id', async () => {
+			const project = await database('project').first();
+			const projectToDelete = project.id
+			
+			const response = await request(app).delete(`/api/v1/project/${projectToDelete}`);
+			const deleted = await database('project').where({ id: projectToDelete })
+
+			expect(response.status).toBe(200);
+			expect(deleted).toEqual([])
+		});
+
+		it('should respond with an error if id doesn\'t exist in the project database', async () => {
+			const expectedError = "{\"error\":\"Could not find project with id: 888\"}";
+
+			const response = await request(app).delete('/api/v1/project/888');
+
+			expect(response.status).toBe(404);
+			expect(response.text).toBe(expectedError);
+		});
 	})
 });
-
-	//DELETE /project/:id:
-
